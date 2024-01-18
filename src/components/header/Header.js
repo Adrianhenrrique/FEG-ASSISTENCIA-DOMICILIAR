2f6e4392ab5e8c54f28a66634c40e232813afbf2
Adrian Henrique <113368090+Adrianhenrrique@users.noreply.github.com>
2024-01-18 09:45:43 -0300

    add NavBarMobile and configured

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,12 @@
 import NavBar from '../NavBar';
+import NavBarMobile from '../NavBarMobile';
 import style from './Header.module.css'
+import { useMediaQuery } from '@chakra-ui/react'
 
 function Header({logo}) {
+
+    const [isLargerThan800] = useMediaQuery('(min-width: 800px)')
+
     return(
         <>
             <header className={style.appHeader}>
@@ -9,7 +14,7 @@ function Header({logo}) {
                     <img src={logo} className={style.logo}/>
                     <h1>F&G ASSISTÊNCIA DOMICILIAR</h1>
                 </div>
-                <NavBar />
+                {isLargerThan800 ? <NavBar/> : <NavBarMobile/>}
             </header>
         </>
     )
