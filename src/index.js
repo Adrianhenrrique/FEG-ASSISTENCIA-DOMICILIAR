7715ba17c8e5cce17b2133779724c8d0d3d42ff2
Adrian Henrique <113368090+Adrianhenrrique@users.noreply.github.com>
2024-01-22 21:51:31 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { inject } from '@vercel/analytics';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { injectSpeedInsights } from '@vercel/speed-insights';
+ 
+injectSpeedInsights();
  
 inject();
-SpeedInsights();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
