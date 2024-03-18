0062d3fad97c26cc5c2d6e686392c0a7aece1e74
Adrian Henrique <113368090+Adrianhenrrique@users.noreply.github.com>
2024-03-18 14:47:14 -0300

    update

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,20 @@
 import style from './Card.module.css';
+import "animate.css/animate.min.css";
+import ScrollAnimation from 'react-animate-on-scroll';
+
 
 function Card({imagen, title, descript, inverted}) {
+    
     return (
-        <div className={`${style.cardContent} ${inverted ? style.inverted : ''}`}>
-            <img src={imagen} className={style.imagenBanner}/>
-            <div className={`${style.content} ${inverted ? style.inverted : ''}`}> 
-                <h2 className={style.titleName}>{title}</h2>
-                <p className={style.descriptText}>{descript}</p>
+        <ScrollAnimation animateIn="animate__backInLeft">
+            <div className={`${style.cardContent} ${inverted ? style.inverted : ''}`}>
+                <img src={imagen} className={style.imagenBanner}/>
+                <div className={`${style.content} ${inverted ? style.inverted : ''}`}> 
+                    <h2 className={style.titleName}>{title}</h2>
+                    <p className={style.descriptText}>{descript}</p>
+                </div>
             </div>
-        </div>
+        </ScrollAnimation>
     )
 }
 
